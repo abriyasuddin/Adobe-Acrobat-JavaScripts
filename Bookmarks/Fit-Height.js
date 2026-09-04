7307e0ce430166c98ecf4291061b085f4a1e6ee6
Riyas <266669925+abriyasuddin@users.noreply.github.com>
2026-09-04 16:27:54 +0400

    Add Fit Height script

---
+++ Bookmarks/Fit-Height.js
@@ -0,0 +1,1 @@
+var d=this;function f(b){if(!b||!b.children)return;for(var i=0;i<b.children.length;i++){var x=b.children[i];x.execute();var p=d.pageNum;x.setAction("this.pageNum="+p+";this.zoomType=zoomtype.fitH;");f(x)}}function s(b){if(!b||!b.children)return;for(var i=0;i<b.children.length;i++){var x=b.children[i];if(x.name=="Standard Drawings")f(x);else s(x)}}s(d.bookmarkRoot);
