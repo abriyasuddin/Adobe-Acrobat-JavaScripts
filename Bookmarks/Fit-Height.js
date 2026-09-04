a490b691b21c4da2dc2408b330ec257ed73425c4
Riyas <266669925+abriyasuddin@users.noreply.github.com>
2026-09-04 16:31:04 +0400

    Modify bookmark target and add user settings comment
    Updated bookmark handling to target 'PARENT BOOKMARK' instead of 'Standard Drawings'. Added user settings comment for clarity.

--- Bookmarks/Fit-Height.js
+++ Bookmarks/Fit-Height.js
@@ -1,1 +1,3 @@
-var d=this;function f(b){if(!b||!b.children)return;for(var i=0;i<b.children.length;i++){var x=b.children[i];x.execute();var p=d.pageNum;x.setAction("this.pageNum="+p+";this.zoomType=zoomtype.fitH;");f(x)}}function s(b){if(!b||!b.children)return;for(var i=0;i<b.children.length;i++){var x=b.children[i];if(x.name=="Standard Drawings")f(x);else s(x)}}s(d.bookmarkRoot);
+var d=this;function f(b){if(!b||!b.children)return;for(var i=0;i<b.children.length;i++){var x=b.children[i];x.execute();var p=d.pageNum;x.setAction("this.pageNum="+p+";this.zoomType=zoomtype.fitH;");f(x)}}function s(b){if(!b||!b.children)return;for(var i=0;i<b.children.length;i++){var x=b.children[i];if(x.name=="PARENT BOOKMARK")f(x);else s(x)}}s(d.bookmarkRoot);
+// ===== USER SETTINGS =====
+// Change this "PARENT BOOKMARK" With Bookmark level you want.
